56beec2d81395ab22db795dfe1e0ad8566b997b6
Khalil <khalil.hersi@hotmail.co.uk>
2023-03-09 20:21:17 +0000

    revoked change in test

--- src/test_main.py
+++ src/test_main.py
@@ -15,5 +15,4 @@ def test_hello_world_message_from_index():
 
 def test_hello_world_message_from_client(client):
     response = client.get('/')
-    assert response.status_code == 200
-    assert b'Hello World' in response.data
+    assert response.data.decode('UTF-8') == 'Hello World'
