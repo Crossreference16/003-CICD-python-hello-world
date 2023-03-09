8747cb5898f68aebeb44a830913fdf0e3c4c4a33
Khalil <khalil.hersi@hotmail.co.uk>
2023-03-09 20:19:45 +0000

    editted test file

--- src/test_main.py
+++ src/test_main.py
@@ -15,4 +15,5 @@ def test_hello_world_message_from_index():
 
 def test_hello_world_message_from_client(client):
     response = client.get('/')
-    assert response.data.decode('UTF-8') == 'Hello World'
+    assert response.status_code == 200
+    assert b'Hello World' in response.data
